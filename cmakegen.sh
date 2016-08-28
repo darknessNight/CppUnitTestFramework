834993544dedcda9c81df42556b47b5e504f7823
Paweł Przytarski <pawelmrocznyp@gmail.com>
2016-08-28 20:01:26 +0200

    Added cmake files

---
+++ cmakegen.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/d/Desktop/skrypty/cmakeAutoGen.sh CppUnitTestFramework /d/Desktop/projekty/LEARN/TDD/CppUnitTestsFramework/CppUnitTestsFramework/ library 
+/d/Desktop/skrypty/cmakeAutoGen.sh CppUnitTestFramework.CompileTest /d/Desktop/projekty/LEARN/TDD/CppUnitTestsFramework/CppUnitTestFramework.CompileTest/ executable .. CppUnitTestFramework
+
