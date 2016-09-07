9881e0028f39ae0696a3846d61dfbe1a86e6868e
Paweł Przytarski <pawelmrocznyp@gmail.com>
2016-09-07 17:43:47 +0200

    Move CategoryContainer to another file

--- Runners/TestsRunner/TestReportContainer.h
+++ Runners/TestsRunner/TestReportContainer.h
@@ -1,38 +1,11 @@
 #pragma once
-#include <CppUnitTestFramework/TestReport.h>
-#include <vector>
-#include <map>
 #include <algorithm>
+#include "TestReportCategoryContainer.h"
 
 using namespace darknessNight::CppUnitTestFramework;
 
 namespace darknessNight {
 	namespace TestsRunner {
-		class CategoryContainer {
-		private:
-			std::map<std::string, CategoryContainer> subcats;
-			std::vector<TestReport> reports;
-		private:
-			void addReportToSubCat(TestCategory index, TestReport& rep) {
-				auto subCat = index.getSubCategory();
-				if (subCat.getName().size() > 0)
-					subcats[index.getName()].addReportToSubCat(index.getSubCategory(), rep);
-				else subcats[index.getName()].reports.push_back(rep);
-			}
-		public:
-			void addReportToCat(TestReport rep) {
-				addReportToSubCat(rep.getCategory(), rep);
-			}
-
-			CategoryContainer& operator[](std::string index) {
-				return subcats[index];
-			}
-
-			TestReport& operator[](int index) {
-				return reports[index];
-			}
-		};
-
 		class TestReportContainer {
 			std::vector<TestReport> reportsArray;
 		public:
